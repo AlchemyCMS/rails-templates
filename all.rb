7e7cbfa391d920289ef13e3ba3fddfe7d62398d4
Thomas von Deyen <thomas@vondeyen.com>
2020-02-26 09:03:29 +0100

    Fix order of install tasks

--- all.rb
+++ all.rb
@@ -2,8 +2,6 @@ gem 'alchemy_cms'
 gem 'alchemy-devise'
 
 after_bundle do
-  rails_command 'alchemy:install'
   generate 'alchemy:devise:install'
-
-  rails_command 'db:migrate'
+  rails_command 'alchemy:install'
 end
